744a50875e57bd5f5a48d5b0f267ec9266845cff
P.angelino <p.angelinodaniele@gmail.com>
2022-05-12 18:52:04 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Controllers/PizzeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.Utils;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> pizze = PizzaData.GetPizze();
+
+            return View("HomePage", pizze);
+        }
+
+        [HttpGet]
+        public IActionResult Details(string name)
+        {
+            Pizza pizzaFound = null;
+
+            foreach(Pizza pizze in PizzaData.GetPizze())
+            {
+                if(pizze.Name == name)
+                {
+                    pizzaFound = pizze;
+                    break;
+                }
+            }
+
+            if (pizzaFound != null)
+            {
+                return View("Details", pizzaFound);
+            }
+            else
+            {
+                return NotFound("La pizza con il nome " + name + " non è stato trovato");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormPizza");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (Pizza nuovaPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPizza", nuovaPizza);
+            }
+
+            PizzaData.GetPizze().Add(nuovaPizza);
+            return RedirectToAction("Index");
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,39 @@
+﻿using la_mia_pizzeria_static.Utils.Validation;
+using System.ComponentModel.DataAnnotations;
+
+
+
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        [Required(ErrorMessage = "Il campo nome è obbligatorio")]
+        [StringLength(20, ErrorMessage = "Il nome non può avere più di 20 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        [MoreThanFiveWords]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "l'URL dell'immagine è obbligatorio")]
+        [Url(ErrorMessage = "Mi dispiace l'URL inserito non è valido")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il prezzo della pizza è obbligatorio")]
+        public double Prezzo { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(string name, string description, string image, double prezzo)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Prezzo = prezzo;
+        }
+    }
+}
