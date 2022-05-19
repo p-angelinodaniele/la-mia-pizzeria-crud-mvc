18f18922b71a965469484c16cdb5f3c0f9c7c6b2
P.angelino <p.angelinodaniele@gmail.com>
2022-05-19 12:03:48 +0200

    axios

---
+++ la-mia-pizzeria-static/Controllers/Api/PizzasController.cs
@@ -0,0 +1,23 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> pizze = new List<Pizza>();
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                pizze = db.Pizze.ToList<Pizza>();   
+            }
+            return Ok(pizze);
+        }
+    }
+}
